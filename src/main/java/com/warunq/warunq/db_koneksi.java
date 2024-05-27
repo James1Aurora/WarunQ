6671f87596c54e30d9aaac9ced30aea4df46b4be
Billdan Satriana <billdansr07@gmail.com>
2024-05-27 16:48:11 +0700

    db_koneksi berhasil terhubung.

--- src/main/java/com/warunq/warunq/db_koneksi.java
+++ src/main/java/com/warunq/warunq/db_koneksi.java
@@ -4,10 +4,36 @@
  */
 package com.warunq.warunq;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Win10
  */
 public class db_koneksi {
     
+    public static Connection koneksi;
+    
+    public static Connection konfigurasi_koneksiDB() throws SQLException {
+        
+        try {
+            String user = "avnadmin";
+            String password = "AVNS_dHcesTccniEeErK2qOH";
+            String url = "jdbc:mysql://mysql-warunq-warunq.d.aivencloud.com:28737/defaultdb?sslmode=require";
+
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            koneksi = DriverManager.getConnection(url, user, password);
+            System.out.println("Database berhasil terhubung!");
+        } catch (SQLException e) {
+            System.err.println("Database gagal terhubung!");
+        }
+        
+        return koneksi;
+    }
+    
+    public static void main(String[] args) throws SQLException {
+        Connection penghubung = (Connection)db_koneksi.konfigurasi_koneksiDB();
+    }
 }
